bb1eb65aa660dfd00726705ab304fc38a9c7b74c
VladimirIvakhnin <v.v.ivakhnin@student.khai.edu>
2026-03-12 17:40:12 +0200

    Create array_utils.h

---
+++ array_utils.h
@@ -0,0 +1,12 @@
+#ifndef ARRAY_UTILS_H
+#define ARRAY_UTILS_H
+
+void getArray(const char filename[], int arr[], int &n);
+
+void saveArray(const char filename[], int arr[], int n);
+
+void modifyArray77(int arr[], int n);
+
+void selectionSort(int arr[], int n);
+
+#endif
