edb02f3b1ddd9778076233395ad2a591ba720587
VladimirIvakhnin <v.v.ivakhnin@student.khai.edu>
2026-03-12 17:41:40 +0200

    Add matrix_utils.h header file with matrix functions

---
+++ matrix_utils.h
@@ -0,0 +1,8 @@
+#ifndef MATRIX_UTILS_H
+#define MATRIX_UTILS_H
+
+void getMatrix(const char filename[], int matrix[20][20], int &rows, int &cols);
+
+void matrix29(int matrix[20][20], int rows, int cols);
+
+#endif
